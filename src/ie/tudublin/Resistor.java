d9cb65a0af66bd43cef890f6ab45b022a0aa918e
Wiktoria <uramowska.wiktoria@gmail.com>
2019-04-10 17:08:29 +0100

    Resistor class added

---
+++ src/ie/tudublin/Resistor.java
@@ -0,0 +1,17 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+
+import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
+
+public class Resistor extends UI
+{
+    int value;
+    int ones;
+    int tens;
+    int hundreds;
+    
+    
+}
