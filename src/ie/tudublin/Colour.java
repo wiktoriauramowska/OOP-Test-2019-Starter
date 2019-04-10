802067229105a343a6d10789c9455875c4bac29c
Wiktoria <uramowska.wiktoria@gmail.com>
2019-04-10 16:23:07 +0100

    Colour class created

---
+++ src/ie/tudublin/Colour.java
@@ -0,0 +1,17 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
+
+
+public class Colour extends PApplet
+
+{
+
+    public void loadColours(){
+        Table table = loadTable("colours.csv", "header");
+        
+    }
+}
